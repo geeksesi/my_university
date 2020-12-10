6d813ceff35b1ab3b0b5bb625f31388841e9b532
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2020-12-10 18:23:27 +0330

    T3.1 & T3.2

---
+++ 991/MyRoleAsTa/ T3/1/answer/answer.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	double A,B,C,D,E,F;
+
+	
+	
+	cin>>A>>B;
+	
+	
+	C=A+B;
+	D=A-B;
+	E=A*B;
+	F=A/B;
+	
+	
+	cout<<C<<"\n";
+	
+	cout<<D<<"\n";
+	
+	cout<<E<<"\n";
+	
+	cout<<F;
+	
+	
+	return 0;
+}
