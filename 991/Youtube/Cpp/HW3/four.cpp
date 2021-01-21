bdeb03e806de9aea7bf3018a935e5a3895767f96
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-01-21 11:22:18 +0330

    A4

---
+++ 991/Youtube/Cpp/HW3/four.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    // if (N < 0)
+    // {
+    //     cout << N * (-1);
+    // }
+    // else
+    // {
+    //     cout << N;
+    // }
+
+    cout << abs(N);
+    return 0;
+}
