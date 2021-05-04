036648eb783284c48935f597b002737ec594244e
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-05-04 06:24:14 +0430

    say hello to 992. fix mr beheshti's problem

---
+++ 992/T1/1/test1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string a,b,d;
+    string c;
+
+    cin>>a;
+    cin>>b;
+    cin>>d;
+    cin>>c;
+
+    cout<<"First Name: "<<a<<endl;
+    cout<<"Last Name: "<<b<<endl;
+    cout<<"Std Major: "<<d<<endl;
+    cout<<"Std ID: "<<c<<endl;
+
+
+    return 0;
+}
