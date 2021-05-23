5baf3bbffa15e61e17223cce94baaa177375ca58
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-05-23 07:16:02 +0430

    new video

---
+++ 991/Youtube/Cpp/HW3/eleven.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iterator>
+
+int main() {
+
+  int a, b;
+  std::cin >> a >> b;
+
+  if (a > b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+  }
+
+  for (int i = a; i > 0; i--) {
+    if (!(a % i) && !(b % i)) {
+      std::cout << "YES " << i << std::endl;
+    }
+  }
+
+  return 0;
+}
